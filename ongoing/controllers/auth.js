30be6f7137ae37f7e12100d1adf2da9700362e30
srikanth019 <srikanth.golla@brainvire.com>
2023-02-23 19:41:53 +0530

    Creating a token and send the email sec-17(280)

--- ongoing/controllers/auth.js
+++ ongoing/controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 // const sendGridTransport = require('nodemailer-sendgrid-transport');
@@ -119,3 +120,54 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     });
 };
+
+exports.getReset = (req,res,next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        pageTitle: 'Reset Password',
+        path: '/reset',
+        errorMessage: message
+    });
+};
+
+exports.postReset = (req,res,next) => {
+    crypto.randomBytes(32, (err,buffer) => { //here 2nd arg is the cb it will be executed once it done.
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({ email: req.body.email})
+        .then(user => {
+            if (!user) {
+                req.flash('error', 'No account is Found with this Email')
+                res.redirect('/reset');
+            }
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + 3600000;
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/');
+            transporter.sendMail({
+                to: req.body.email,
+                from: 'mailto:srikanth.golla@brainvire.com',
+                subject: "Password Reset",
+                text: 'Hello from Node-Project Password Resetting',
+                html: `
+                    <p>You requested password reset</p>
+                    <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to reset user password.</p>
+                `
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    })
+}
--- ongoing/models/user.js
+++ ongoing/models/user.js
@@ -11,6 +11,8 @@ const mongoose = require('mongoose');
         type: String,
         required: true
     },
+    resetToken: String,
+    resetTokenExpiration: Date,
     cart : {
         items: [
             { 
